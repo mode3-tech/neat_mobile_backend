01e8165eacefe9d3781a19724d898354fee781de
Twith2e <abdulbasitadebajo1619@gmail.com>
2026-03-06 21:30:38 +0100

    fix: done, now enforcing not null again

--- models/user.go
+++ models/user.go
@@ -4,14 +4,14 @@ import "time"
 
 type User struct {
 	ID              string    `gorm:"column:id;primaryKey;index"`
-	Email           string    `gorm:"column:email;unique"`
-	Phone           string    `gorm:"column:phone;unique"`
-	PasswordHash    string    `gorm:"column:password"`
-	PinHash         string    `gorm:"column:pin_hash"`
+	Email           string    `gorm:"column:email;unique;not null"`
+	Phone           string    `gorm:"column:phone;unique;index;not null"`
+	PasswordHash    string    `gorm:"column:password;not null"`
+	PinHash         string    `gorm:"column:pin_hash;not null"`
 	IsEmailVerified bool      `gorm:"is_email_verified"`
-	IsPhoneVerified bool      `gorm:"is_phone_verified"`
-	IsBvnVerified   bool      `gorm:"is_bvn_verified"`
-	IsNinVerified   bool      `gorm:"is_nin_verified"`
+	IsPhoneVerified bool      `gorm:"is_phone_verified;not null"`
+	IsBvnVerified   bool      `gorm:"is_bvn_verified;not null"`
+	IsNinVerified   bool      `gorm:"is_nin_verified;not null"`
 	CreatedAt       time.Time `gorm:"column:created_at;autoCreateTime"`
 }
 
