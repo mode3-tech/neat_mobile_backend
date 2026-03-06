3d6c72b7b513fd502d525b9d299ee3cb29feb896
Twith2e <abdulbasitadebajo1619@gmail.com>
2026-03-06 21:25:16 +0100

    fix: pg issue testing for resolvement2

--- models/user.go
+++ models/user.go
@@ -5,13 +5,13 @@ import "time"
 type User struct {
 	ID              string    `gorm:"column:id;primaryKey;index"`
 	Email           string    `gorm:"column:email;unique"`
-	Phone           string    `gorm:"column:phone;unique;not null"`
-	PasswordHash    string    `gorm:"column:password;not null"`
+	Phone           string    `gorm:"column:phone;unique"`
+	PasswordHash    string    `gorm:"column:password"`
 	PinHash         string    `gorm:"column:pin_hash"`
 	IsEmailVerified bool      `gorm:"is_email_verified"`
-	IsPhoneVerified bool      `gorm:"is_phone_verified;not null"`
-	IsBvnVerified   bool      `gorm:"is_bvn_verified;not null"`
-	IsNinVerified   bool      `gorm:"is_nin_verified;not null"`
+	IsPhoneVerified bool      `gorm:"is_phone_verified"`
+	IsBvnVerified   bool      `gorm:"is_bvn_verified"`
+	IsNinVerified   bool      `gorm:"is_nin_verified"`
 	CreatedAt       time.Time `gorm:"column:created_at;autoCreateTime"`
 }
 
