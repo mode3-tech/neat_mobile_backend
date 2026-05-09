6bd52544cc12adc2843a706a9914bfe7af7b94c1
Twith2e <abdulbasitadebajo1619@gmail.com>
2026-05-09 01:43:00 +0100

    fix: error resolved

--- providers/baas/optimus.go
+++ providers/baas/optimus.go
@@ -15,4 +15,4 @@ func NewOptimus(baseURL string, apiKey string) *Optimus {
 	return &Optimus{BaseURL: baseURL, ApiKey: apiKey, Client: &http.Client{Timeout: time.Second * 15}}
 }
 
-func (o *Optimus) GenerateWallet()
+// func (o *Optimus) GenerateWallet()
