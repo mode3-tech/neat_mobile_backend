fe77dff11984364a93dd412a2991552a4f32cee2
Twith2e <abdulbasitadebajo1619@gmail.com>
2026-09-23 11:41:40 +0100

    fix(audit log): scanning into metadata bug fixed

--- internal/modules/audit_log/models.go
+++ internal/modules/audit_log/models.go
@@ -1,21 +1,22 @@
 package auditlog
 
 import (
+	"neat_mobile_app_backend/internal/types"
 	"time"
 )
 
 type AuditLog struct {
-	ID           string                 `gorm:"primaryKey"`
-	Timestamp    time.Time              `gorm:"index;not null"`
-	ActorType    string                 `gorm:"not null"`
-	ActorID      string                 `gorm:"index;not null"`
-	Action       string                 `gorm:"index;not null"`
-	ResourceType ResourceType           `gorm:"index:idx_audit_resource;check:resource_type IN ('wallet', 'user', 'user_account', 'transaction', 'loan', 'loan_application', 'tier_upgrade', 'device', 'session', 'nin', 'bvn', 'beneficiary', 'card', 'savings_goal')"`
-	ResourceID   string                 `gorm:"index:idx_audit_resource;not null"`
-	Status       LogStatus              `gorm:"not null"`
-	RequestID    string                 `gorm:"index;not null"`
-	IPAddress    string                 `gorm:"not null"`
-	Metadata     map[string]interface{} `gorm:"type:jsonb;"`
+	ID           string        `gorm:"primaryKey"`
+	Timestamp    time.Time     `gorm:"index;not null"`
+	ActorType    string        `gorm:"not null"`
+	ActorID      string        `gorm:"index;not null"`
+	Action       string        `gorm:"index;not null"`
+	ResourceType ResourceType  `gorm:"index:idx_audit_resource;check:resource_type IN ('wallet', 'user', 'user_account', 'transaction', 'loan', 'loan_application', 'tier_upgrade', 'device', 'session', 'nin', 'bvn', 'beneficiary', 'card', 'savings_goal')"`
+	ResourceID   string        `gorm:"index:idx_audit_resource;not null"`
+	Status       LogStatus     `gorm:"not null"`
+	RequestID    string        `gorm:"index;not null"`
+	IPAddress    string        `gorm:"not null"`
+	Metadata     types.JSONMap `gorm:"type:jsonb;"`
 }
 
 func (AuditLog) TableName() string {
